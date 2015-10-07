1b1295d145f6414857207b56f2ea76d8e2c1cd18
TomonoriMinegishi <krocwercs@gmail.com>
2015-10-07 17:12:27 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
-  def new
+  def index
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Ruby on Rails!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def delete
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end

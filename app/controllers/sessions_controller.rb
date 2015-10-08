f10622a546e1650ac4c70ac96ec69b8e89f284a1
TomonoriMinegishi <krocwercs@gmail.com>
2015-10-08 19:32:42 +0900

    Finished 8.4.3

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #redirect to login page
+      log_in user
+      remember user
+      redirect_to user
+    else
+      #error message
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end

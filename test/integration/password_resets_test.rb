c1ab60f7ae3cebd9c88ff9b908f7786c90c0d59a
TomonoriMinegishi <krocwercs@gmail.com>
2015-10-14 13:58:24 +0900

    Finished chapter 10

---
+++ test/integration/password_resets_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class PasswordResetsTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+    @user = users(:tomonori)
+  end
+
+  test "password resets" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    # invalid mailaddress
+    post password_resets_path, password_reset: { email: "" }
+    assert_not flash.empty?
+    assert_template 'password_resets/new'
+    # valid mailaddress
+    post password_resets_path, password_reset: { email: @user.email }
+    assert_not_equal @user.reset_digest, @user.reload.reset_digest
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_not flash.empty?
+    assert_redirected_to root_url
+    # password reset form
+    user = assigns(:user)
+    # invlid mailaddress
+    get edit_password_reset_path(user.reset_token, email: "")
+    assert_redirected_to root_url
+    # invalid user
+    user.toggle!(:activated)
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_redirected_to root_url
+    user.toggle!(:activated)
+    # valid mailaddress, invalid token
+    get edit_password_reset_path('wrong token', email: user.email)
+    assert_redirected_to root_url
+    # valid mailaddress, valid token
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_template 'password_resets/edit'
+    assert_select "input[name=email][type=hidden][value=?]", user.email
+    # invalid password
+    patch password_reset_path(user.reset_token), email: user.email, user: { password: "foobaz",
+                                                                            password_confirmation: "barquux" }
+    assert_select 'div#error_explanation'
+    # empty password
+    patch password_reset_path(user.reset_token), email: user.email, user: { password: "",
+                                                                          password_confirmation: "" }
+    assert_select 'div#error_explanation'
+    # valid password
+    patch password_reset_path(user.reset_token), email: user.email, user: { password: "foobaz",
+                                                                          password_confirmation: "foobaz" }
+    assert is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to user
+  end
+end

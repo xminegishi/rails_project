cbbcccfd4f0f6f785d7e5cf09f6ec4a23a83afac
TomonoriMinegishi <krocwercs@gmail.com>
2015-10-22 19:18:28 +0900

    Modified title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "RailsProject"
+    base_title = "Thermopile PJ"
     if page_title.empty? 
       base_title
     else

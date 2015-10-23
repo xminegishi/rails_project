8a7cf6a2bc0901b2b6f264a12018b705e9512e6a
TomonoriMinegishi <krocwercs@gmail.com>
2015-10-23 17:19:33 +0900

    Arranged home view

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Thermopile PJ"
+    assert_select "title", "Thermopile Project"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "HELP | Thermopile PJ"
+    assert_select "title", "HELP | Thermopile Project"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "ABOUT | Thermopile PJ"
+    assert_select "title", "ABOUT | Thermopile Project"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "CONTACT | Thermopile PJ"
+    assert_select "title", "CONTACT | Thermopile Project"
   end
 end
